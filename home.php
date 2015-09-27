fce554f7822cb72bcaa4bc7cab7a82f2b00c1a2b
solution-man <vahid7m@gmail.com>
2015-09-27 23:07:18 +0330

    ok

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+//$courses = array('me','you','him');
+$courses = ['i', 'uou'];
+echo $courses[1];
+//var_dump($courses);
+//print_r($courses);
+//var_dump($courses);
+
+?>
+
+
+</body>
+</html>
+
